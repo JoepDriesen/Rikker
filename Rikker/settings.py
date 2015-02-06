620f2f1a681cf1d74986b54a7c9e869f6a7419e2
Joep Driesen <joep@seasoning.be>
2015-02-06 16:35:56 +0100

    some bfs

--- Rikker/settings.py
+++ Rikker/settings.py
@@ -76,8 +76,6 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, 'Rikker/static'),
-    os.path.join(BASE_DIR, 'portal/static'),
 )
 
 # List of finder classes that know how to find static files in
